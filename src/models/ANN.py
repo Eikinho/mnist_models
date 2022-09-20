f935ef17482c08c3739b14bd16f1f6ee42fdeede
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2022-09-20 01:31:15 -0300

    update

--- src/models/ANN.py
+++ src/models/ANN.py
@@ -9,9 +9,9 @@ class ANN(Net):
         super().__init__(model=tf.keras.models.Sequential(), name="ANN")
         self.model.add(Flatten())
         self.model.add(Dense(256, activation=tf.nn.relu))
-        self.model.add(Dropout(0.2))
+        self.model.add(Dropout(0.4))
         self.model.add(Dense(128, activation=tf.nn.relu))
-        self.model.add(Dropout(0.2))
+        self.model.add(Dropout(0.4))
         self.model.add(Dense(10, activation=tf.nn.softmax))
         self.model.compile(
             optimizer="adam",
