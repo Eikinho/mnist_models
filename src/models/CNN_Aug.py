bf5c618bdbde1d7968a6e98be3b285d394031a4f
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2022-09-20 01:21:04 -0300

    update

--- src/models/CNN_Aug.py
+++ src/models/CNN_Aug.py
@@ -7,13 +7,17 @@ from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout
 class CNN_Aug(Net):
     def __init__(self):
         super().__init__(model=tf.keras.models.Sequential(), name="CNN_Aug")
-        self.model.add(Conv2D(13, (3, 3), activation=tf.nn.relu, input_shape=(28, 28, 1)))
+        self.model.add(Conv2D(64, (7, 7), activation=tf.nn.relu, input_shape=(28, 28, 1)))
         self.model.add(MaxPooling2D(2, 2))
-        self.model.add(Conv2D(5, (3, 3), activation=tf.nn.relu))
+        self.model.add(Conv2D(128, (7, 7), activation=tf.nn.relu))
+        self.model.add(Dropout(0.2))
+        self.model.add(MaxPooling2D(2, 2))
+        self.model.add(Conv2D(256, (7, 7), activation=tf.nn.relu))
         self.model.add(Dropout(0.2))
         self.model.add(MaxPooling2D(2, 2))
         self.model.add(Flatten())
         self.model.add(Dense(64, activation=tf.nn.relu))
+        self.model.add(Dropout(0.2))
         self.model.add(Dense(10, activation=tf.nn.softmax))
         self.model.compile(
             optimizer="adam",
